323c73c294adb8be5ca7f5b92d18091626f27359
gabrieljim <gabrieljim641@gmail.com>
2019-07-05 23:05:29 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+    return(
+        <button className="square">
+            {props.value}
+        </button>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+    renderSquare(i) {
+        return(
+            <Square id={i} value={this.props.squares[i]} onClick={(i) => this.props.onClick(i)}/>
+        )
+    }
+
+    render() {
+        return(
+            <div>
+                <div className="row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
+}
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        let testArray=Array(9).fill(null);
+        testArray[4]="X";
+        this.state = {
+            squares: testArray,
+            xTurn: true,
+        }
+    }
+
+    handleClick(i) {
+        return;
+    }
+
+    render() {
+        return(
+            <Board squares={this.state.squares} onClick={(i) => this.handleClick(i)}/>
+        )
+    }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"))
